3a80a2536cdf420c6f45a81adf614a56972e6f7d
macbric <macbric>
2014-04-08 11:12:18 +0200

    Ajout -v

--- main.cpp
+++ main.cpp
@@ -51,6 +51,58 @@ int main(int argc, char ** argv)
 		}
 		else if(strcmp(argv[1],"-v") == 0)
 		{
+			if(argc == 4)
+			{
+				FILE * fid;
+				fid = fopen(argv[2],"r");
+				if (!fid)
+				{
+					cerr<<"Unable to open file "<<argv[2]<<endl;
+					return 1;
+				}
+				xmlin = fid;
+				Document * document;
+				int retour = xmlparse(&document);
+				if (!retour)
+				{
+					FILE * fid2;
+					fid2 = fopen(argv[3],"r");
+					if (!fid2)
+					{
+						cerr<<"Unable to open file "<<argv[3]<<endl;
+						return 1;
+					}
+					xmlin = fid2;
+					Document * document2;
+					int retourXsd = xmlparse(&document2);
+					if (!retourXsd)
+					{
+					  //document->display();
+					  //document2->display();
+					////
+						// Validation XSD
+					////
+
+					  delete document;
+					  delete document2;
+					  return 0;
+					}
+					else
+					{
+						cerr<<"No root markup"<<endl;
+						return 1;
+					}
+				}
+				else
+				{
+					cerr<<"No root markup"<<endl;
+					return 1;
+				}
+			}
+			else
+			{
+				cerr<<"You must provide an argument to the command -v"<<endl;
+			}
 		}
 		else if(strcmp(argv[1],"-t") == 0)
 		{
@@ -77,5 +129,6 @@ int main(int argc, char ** argv)
 		cerr<<"../xmltool -t file.xml file.xsl : parse both xml and xsl files and display de transformation result of file.xml by the stylesheet file.xsl"<<endl;
 		cerr<<"../xmltool -h : displays this help"<<endl;
 	}
+
 	return 1;
 }
