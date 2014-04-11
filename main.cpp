95f11ef78ac6e007be9a36e611969021c67be928
JulienCumin <julien.cumin@insa-lyon.fr>
2014-04-11 11:15:20 +0200

    Modifs fuite mémoire

--- main.cpp
+++ main.cpp
@@ -140,7 +140,6 @@ int main(int argc, char ** argv)
 						Element *docTransf = templ->traiterTemplate(racineXML, docXLS->getElement(), racineXML);
 						docTransf->displayResultatTransf();
 						
-						delete docTransf;
 						delete docXML;
 						delete docXLS;
 						return 0;
